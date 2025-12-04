dddeb18edf3ef8d08789b8eb83dc3e1d4cf0d02c
Divyanshukumawat <divyanshukumawat0478@gmail.com>
2025-12-04 19:20:49 +0530

    correction

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -491,7 +491,7 @@ def all_clients():
             FROM client_payment_details
             GROUP BY cw_id
             ) p ON cw.cw_id = p.cw_id
-            WHERE cw.status='Client order by due_amount'
+            WHERE cw.status='Client' order by due_amount desc
             
         """)
     ).mappings().all()
